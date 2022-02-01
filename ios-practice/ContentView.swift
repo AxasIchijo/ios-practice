40c26eba5337d886d93501a315311b1c45d1b18e
Ichijo Hiroki <hiroki.ichijo@axas-japan.co.jp>
2022-02-01 20:44:45 +0900

    Initial Commit

---
+++ ios-practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios-practice
+//
+//  Created by AXLT0221-AP on 2022/02/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios-practice/ios_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_practiceApp.swift
+//  ios-practice
+//
+//  Created by AXLT0221-AP on 2022/02/01.
+//
+
+import SwiftUI
+
+@main
+struct ios_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
